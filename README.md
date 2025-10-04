2dcaa3fdea82a2f2bb96f4c7b981726f2c919adf
OErickBrunoo <erickferreira2729@gmail.com>
2025-10-04 11:46:14 -0300

    Adição de várias funções

--- README.md
+++ README.md
@@ -1,13 +1,46 @@
-Uma calculadora C com uma interface bem simples em html usando a biblioteca <stdlib.h> do C e a compilação em CGI (Comom Getway Interface) para que tenha cominicação entre a interface web, mais especificamente javascript e código C.
+Uma calculadora em C com uma interface bem simples em HTML, utilizando a biblioteca <stdlib.h> e compilação via CGI (Common Gateway Interface). Isso permite que haja comunicação entre a interface web e o código em C.
 
-O javascript manda a informação necessária em uma strig via url para a calculadora.cgi, já que cgi é uma compilação reconhecida por ele, então ele sabe como e para onde mandar. Ele manda no seguinte formato: {num1}&${operador}&${num2}.
+O JavaScript envia as informações necessárias em uma string, via URL, para o script calculadora.cgi. Ele manda os dados no seguinte formato: {num1}&{operador}&{num2}.
 
-O stdlib.h é uma biblioteca de alocação de memória que pega essa string, então ao usar o getenvi() que pega no local de memória padrão "QUERY_STRING" para colocar na variavel dados.
+A biblioteca <stdlib.h> fornece a função getenv(), que é usada para capturar essa string. A função lê a variável de ambiente padrão "QUERY_STRING" e armazena seu conteúdo na variável dados.
 
+Também existe uma versão da calculadora (calculadoraindependente.c) que possui as mesmas funções, mas funciona de forma autônoma, com toda a interatividade via console.
+
+Sobre o funcionamento da Calculadora em si:
+
+Para a escolha de função foi usado o switch case, e para o funcionamento da vasta maioria das funções foi usando as funções da biblioteca <math.h>.
+
+Funções:
+
+- Adição
+- Subtração
+- Multiplicação
+- Divisão
+- Exponenciação (Potência)
+- Módulo (Resto da divisão)
+- Porcentagem
+- Hipotenusa
+- Raiz quadrada
+- Raiz cúbica
+- Seno
+- Cosseno
+- Tangente
+- Arco seno
+- Arco cosseno
+- Arco tangente
+- Logaritmo na base 10
+- Logaritmo natural
+- Fatorial
+- Valor absoluto
+- Arredondar para baixo
+- Arredondar para cima
+- Converter Graus para radianos
+- Converter Radianos para graus
+- Equação de segundo grau (Bhaskara)
 
 Dependências:
 - python
-
+- MinGW
 
 Como Executar:
 
--- calculadoraIndependente.c
+++ calculadoraIndependente.c
@@ -3,18 +3,52 @@
 #include <stdlib.h>
 
 int main() {
-
-	printf("Content-Type: text/plain\n\n");	
-
     double pnumero, snumero, tnumero, qnumero, delta, resultado, resultado2, pcima1, pbaixo, pcima2;
-	float pri_grau, radianos;
-    int escolha;
-	const double pi = 3.1415926535897;
-    char *dados = getenv("QUERY_STRING"); // String exemplo :: 
-	
-	
-    sscanf(dados, "%lf&%d&%lf&%lf", &pnumero, &escolha, &snumero, &tnumero); // formato {num1}&${operador}&${num2}&${num3}
-    
+    int escolha, continuar = 0;
+    const double pi = 3.1415926535897;
+
+
+	while (continuar <= 1) {
+		
+    printf("\n===========================================================================\n");
+    printf("                          CALCULADORA CIENTIFICA\n");
+    printf("===========================================================================\n\n");
+    printf("  [ 1] Adicao             [ 2] Subtracao             [ 3] Multiplicacao\n");
+    printf("  [ 4] Divisao            [ 5] Exponenciacao         [ 6] Modulo (Resto)\n");
+    printf("  [ 7] Porcentagem        [ 8] Hipotenusa            [ 9] Raiz Quadrada\n");
+    printf("  [10] Raiz Cubica        [11] Seno                  [12] Cosseno\n");
+    printf("  [13] Tangente           [14] Arcseno               [15] Arccosseno\n");
+    printf("  [16] Arctangente        [17] Logaritmo base 10     [18] Logaritmo Natural\n");
+    printf("  [19] Fatorial           [20] Valor Absoluto        [21] Arredondar p Baixo\n");
+    printf("  [22] Arredondar p Cima  [23] Graus para Radianos   [24] Radianos para Graus\n");
+    printf("  [25] Bhaskara\n\n");
+    printf("===========================================================================\n\n");
+
+
+    printf("Digite o numero da operacao desejada: ");
+    scanf("%d", &escolha);
+
+
+    if (escolha <= 8) {
+        printf("Digite o primeiro numero: ");
+        scanf("%lf", &pnumero);
+        printf("Digite o segundo numero: ");
+        scanf("%lf", &snumero);
+    } 
+    else if (escolha >= 9 && escolha <= 24) {
+        printf("Digite o numero: ");
+        scanf("%lf", &pnumero);
+    }
+	else if (escolha >= 24) {
+		printf("Digite o A: ");
+		scanf("%lf", &pnumero);
+		printf("Digite o B: ");
+		scanf("%lf", &snumero);
+		printf("Digite o C: ");
+		scanf("%lf", &tnumero);
+	}
+
+
     switch (escolha) {
         case 1:// Adição
             resultado = pnumero + snumero;
@@ -22,7 +56,7 @@ int main() {
             break;
         case 2:// Subtração
             resultado = pnumero - snumero;
-            printf("\nresultado: %g\n", resultado);
+            printf("\nResultado: %g\n", resultado);
             break;
         case 3:// Multiplicação
             resultado = pnumero * snumero;
@@ -40,30 +74,30 @@ int main() {
             resultado = pow(pnumero, snumero);
             printf("\nResultado: %g\n", resultado);
             break;
-        case 6:// Modulo resto da divisão
+        case 6:// Módulo resto da divisão
             resultado = fmod(pnumero, snumero);
             printf("\nResultado: %g\n", resultado);
             break;
-        case 7:// Porcentagem
+        case 7:// Porcentagem (pnumero % de snumero)
             resultado = (pnumero / 100.0) * snumero;
-            printf("\nresultado: %g\n", resultado);
+            printf("\nResultado: %g\n", resultado);
             break;
         case 8:// Hipotenusa
             resultado = hypot(pnumero, snumero);
             printf("\nResultado: %g\n", resultado);
             break;
-        case 9:// Raiz quadrada
+        case 9:// Raiz Quadrada
                 resultado = sqrt(pnumero);
                 printf("\nResultado: %g\n", resultado);
             break;
-        case 10:// Raiz cubica
+        case 10:// Raiz Cúbica
             resultado = cbrt(pnumero);
             printf("\nResultado: %g\n", resultado);
             break;
         case 11:{// Seno
             double radianos = pnumero * (pi / 180.0);
             resultado = sin(radianos);
-            printf("\nresultado: %g\n", resultado);
+            printf("\nResultado: %g\n", resultado);
 		}
             break;
         case 12:{// Cosseno
@@ -72,7 +106,7 @@ int main() {
             printf("\nResultado: %g\n", resultado);
 		}
             break;
-        case 13:{ // Tangente
+        case 13:{// Tangente
             double radianos = pnumero * (pi / 180.0);
                 resultado = tan(radianos);
                 printf("\nResultado: %g\n", resultado);
@@ -94,7 +128,7 @@ int main() {
                 resultado = log10(pnumero);
                 printf("\nResultado: %g\n", resultado);
             break;
-        case 18:// Logaritmo natural
+        case 18:// Logaritmo Natural
                 resultado = log(pnumero);
                 printf("\nResultado: %g\n", resultado);
             break;
@@ -105,7 +139,7 @@ int main() {
                 }
                 printf("\nResultado: %g\n", resultado);
             break;
-        case 20:// Valor absoluto
+        case 20:// Valor Absoluto
             resultado = fabs(pnumero);
             printf("\nResultado: %g\n", resultado);
             break;
@@ -113,17 +147,17 @@ int main() {
             resultado = floor(pnumero);
             printf("\nResultado: %g\n", resultado);
             break;
-        case 22:// Arredondar 
+        case 22:// Arredondar para cima
             resultado = ceil(pnumero);
-            printf("\nresultado: %g\n", resultado);
+            printf("\nResultado: %g\n", resultado);
             break;
-        case 23:// Graus para radianos
+        case 23:// Graus para Radianos
             resultado = pnumero * (pi / 180.0);
-            printf("\nResultado radianos: %g\n", resultado);
+            printf("\nResultado (em radianos): %g\n", resultado);
             break;
-        case 24:// Radianos para graus
+        case 24:// Radianos para Graus
             resultado = pnumero * (180.0 / pi);
-            printf("\nResultado graus: %g\n", resultado);
+            printf("\nResultado (em graus): %g\n", resultado);
             break;
         case 25:// Equação de segundo grau usando baskara
 			delta = pow(snumero, 2) - (4 * (pnumero * tnumero));
@@ -139,12 +173,12 @@ int main() {
 			} else if (delta == 0) {
 			resultado = -snumero / (2 * pnumero);
         
-			printf("A equacao possui uma unica raiz real:\n");
+			printf("A equacao possui uma unica raiz real (ou duas raizes iguais):\n");
 			printf("x = %.2lf\n", resultado);
 				
 			} else if (delta < 0) {
-			printf("Delta negativo.\n");
-			printf("A raiz não está nos números reais.\n");
+			printf("O Delta eh negativo.\n");
+			printf("A equacao nao possui raizes no conjunto dos numeros reais.\n");
 				
 			}
 
@@ -153,5 +187,9 @@ int main() {
             printf("\nOperação invalida ou não implementada.\n");
     }
     
+	printf("\n Continuar Calculando?\n 1 para sim \n 2 para não \n Resposta: ");
+	scanf("%d", &continuar);
+	
+	}
     return 0;
 }
